06818cb5d70a660a9e9cbd1b287948a4f70ff420
Juliano Francener <juliano@pij.local>
2026-08-05 12:16:36 -0300

    Adjust Docker volume mount and database path for permission fixes

--- core/settings.py
+++ core/settings.py
@@ -70,13 +70,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
+import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data' / 'db.sqlite3' if os.path.exists(BASE_DIR / 'data') else BASE_DIR / 'db.sqlite3',
     }
 }
 
